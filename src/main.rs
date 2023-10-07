2546ae4b92ec239851539a2ef03cacce35d5a9e5
Lan <catsalwaysmeow@outlook.com>
2023-10-08 03:25:23 +0800

    use resolution-aware icons & fix typo

--- src/main.rs
+++ src/main.rs
@@ -99,7 +99,7 @@ fn logged_main(app_path: Option<&Path>) -> Result<()> {
             let Ok(evt) = rx.recv() else { break };
             match evt {
                 Event::Exit => {
-                    drop(tray); // dead lock: we MUST drop 'tray' here as it relis on the message bump of main thread.
+                    drop(tray); // dead lock: we MUST drop 'tray' here as it relis on the message pump of main thread.
                     match unsafe { PostThreadMessageW(tid, WM_QUIT, WPARAM(0), LPARAM(0)) }.warn() {
                         Some(_) => break,
                         None => process::exit(-1),
@@ -122,11 +122,11 @@ fn logged_main(app_path: Option<&Path>) -> Result<()> {
             }
         });
 
-        let mut msg: MSG = unsafe { mem::zeroed() };
+        let mut msg: MSG = unsafe { mem::zeroed() }; //1
         loop {
             let hr = unsafe { GetMessageW(&mut msg, HWND(0), 0, 0) };
             if matches!(hr, BOOL(0 | -1)) {
-                // note: -1 is an error state but is also unreachable here so we don't handle it.
+                // note: -1 is an error state but is unreachable here so we don't handle it.
                 break;
             }
 
