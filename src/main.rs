035a10eb948fdbaa29d6c76e54847d55a17e83ea
Lan <catsalwaysmeow@outlook.com>
2023-10-14 07:02:03 +0800

    add 256x256 icons & add DPI/color detection

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![windows_subsystem = "windows"]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use std::{env, mem, path::Path, process, sync::mpsc, thread};
 
 use anyhow::{Context, Result};
@@ -13,7 +13,8 @@ use windows::Win32::{
         Input::KeyboardAndMouse::{RegisterHotKey, MOD_CONTROL, VK_OEM_3},
         WindowsAndMessaging::{
             DispatchMessageW, GetForegroundWindow, GetMessageW, GetWindowTextW, PostMessageA,
-            PostThreadMessageW, TranslateMessage, MSG, WM_HOTKEY, WM_KEYDOWN, WM_KEYUP, WM_QUIT,
+            PostThreadMessageW, TranslateMessage, MSG, WM_DPICHANGED,
+            WM_DWMCOLORIZATIONCOLORCHANGED, WM_HOTKEY, WM_KEYDOWN, WM_KEYUP, WM_QUIT,
         },
     },
 };
@@ -26,9 +27,13 @@ const PACKAGE_VERSION: &'static str = env!("CARGO_PKG_VERSION");
 enum Event {
     Exit,
     AutoLaunch,
+    SystemDpiChanged,
+    SystemColorChanged,
 }
 
 fn main() -> Result<()> {
+    windows_dpi::enable_dpi();
+
     let app_path = env::current_exe();
     let file_appender = tracing_appender::rolling::never(
         app_path
@@ -77,9 +82,10 @@ fn logged_main(app_path: Option<&Path>) -> Result<()> {
                 .warn()
         });
     let (tx, rx) = mpsc::channel::<Event>();
+    let mut icon_param = get_icon_param();
     let mut tray: trayicon::TrayIcon<Event> = TrayIconBuilder::new()
         .sender(tx.clone())
-        .icon(get_icon())
+        .icon(select_icon(icon_param))
         .tooltip("Fixing the issue where 「Ctrl+`」 doesn't work with some CJK keyboards/IMEs in VSCode. ")
         .menu(
             MenuBuilder::new()
@@ -119,6 +125,13 @@ fn logged_main(app_path: Option<&Path>) -> Result<()> {
                         }
                     });
                 }
+                Event::SystemDpiChanged | Event::SystemColorChanged => {
+                    let new_icon_param = get_icon_param();
+                    if icon_param != new_icon_param {
+                        icon_param = new_icon_param;
+                        tray.set_icon(&select_icon(icon_param)).warn();
+                    }
+                }
             }
         });
 
@@ -133,6 +146,12 @@ fn logged_main(app_path: Option<&Path>) -> Result<()> {
             match msg.message {
                 WM_HOTKEY if matches!(msg.wParam, WPARAM(KEYID_CTRL_OEM_3)) => {
                     mock_key_press();
+                }
+                WM_DPICHANGED => {
+                    tx.send(Event::SystemDpiChanged).ok();
+                }
+                WM_DWMCOLORIZATIONCOLORCHANGED => {
+                    tx.send(Event::SystemColorChanged).ok();
                 }
                 _unhandled_message => unsafe {
                     TranslateMessage(&msg);
@@ -177,16 +196,69 @@ fn mock_key_press() {
     }
 }
 
-fn get_icon() -> Icon {
+#[derive(Debug, Default, PartialEq, PartialOrd, Clone, Copy)]
+struct IconParam {
+    light_mode: bool,
+    scaling_factor: f32,
+}
+
+fn get_icon_param() -> IconParam {
     let hkcu = RegKey::predef(HKEY_CURRENT_USER);
-    let apps_use_light_theme: Option<u32> = hkcu
+    let system_uses_light_theme: Option<u32> = hkcu
         .open_subkey(r#"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize"#)
         .warn()
-        .and_then(|personalize| personalize.get_value(r#"AppsUseLightTheme"#).warn());
+        .and_then(|personalize| personalize.get_value(r#"SystemUsesLightTheme"#).warn());
 
-    match apps_use_light_theme {
-        Some(1) => icon_light(),
-        _ => icon_dark(),
+    IconParam {
+        light_mode: matches!(system_uses_light_theme, Some(0)),
+        scaling_factor: windows_dpi::desktop_dpi(),
+    }
+}
+
+#[cfg_attr(test, deny(warnings))]
+fn select_icon(
+    IconParam {
+        light_mode,
+        scaling_factor,
+    }: IconParam,
+) -> Icon {
+    let l16: &[u8] = include_bytes!(r#"..\assets\terminal_box_icon-light-16x16.ico"#);
+    let l24: &[u8] = include_bytes!(r#"..\assets\terminal_box_icon-light-24x24.ico"#);
+    let l32: &[u8] = include_bytes!(r#"..\assets\terminal_box_icon-light-32x32.ico"#);
+    let l48: &[u8] = include_bytes!(r#"..\assets\terminal_box_icon-light-48x48.ico"#);
+    let l256: &[u8] = include_bytes!(r#"..\assets\terminal_box_icon-light-256x256.ico"#);
+
+    let d16: &[u8] = include_bytes!(r#"..\assets\terminal_box_icon-dark-16x16.ico"#);
+    let d24: &[u8] = include_bytes!(r#"..\assets\terminal_box_icon-dark-24x24.ico"#);
+    let d32: &[u8] = include_bytes!(r#"..\assets\terminal_box_icon-dark-32x32.ico"#);
+    let d48: &[u8] = include_bytes!(r#"..\assets\terminal_box_icon-dark-48x48.ico"#);
+    let d256: &[u8] = include_bytes!(r#"..\assets\terminal_box_icon-dark-256x256.ico"#);
+
+    #[cfg(test)]
+    {
+        let _ = (light_mode, scaling_factor);
+        [l16, l24, l32, l48, l256, d16, d24, d32, d48, d256]
+            .into_iter()
+            .map(|data| Icon::from_buffer(data, None, None).unwrap())
+            .collect::<Vec<_>>()
+            .pop()
+            .unwrap()
+    }
+
+    #[cfg(not(test))]
+    {
+        let icons = if light_mode {
+            [(16, l16), (24, l24), (32, l32), (48, l48), (256, l256)]
+        } else {
+            [(16, d16), (24, d24), (32, d32), (48, d48), (256, d256)]
+        };
+        let target_size = scaling_factor * 16f32;
+        let (_size, data) = icons
+            .into_iter()
+            .min_by_key(|&(size, ..)| (size as f32 - target_size).abs() as i32)
+            .unwrap(); // unwrap: safe as 'icons' is not empty.
+
+        Icon::from_buffer(data, None, None).unwrap() // unwrap: tested.
     }
 }
 
@@ -203,31 +275,12 @@ impl<T, E: std::fmt::Debug> LogExt<T> for std::result::Result<T, E> {
     }
 }
 
-fn icon_light() -> Icon {
-    Icon::from_buffer(
-        include_bytes!(r#"..\assets\terminal_box_icon-light-48x48.ico"#),
-        None,
-        None,
-    )
-    .unwrap() // unwrap: tested
-}
-
-fn icon_dark() -> Icon {
-    Icon::from_buffer(
-        include_bytes!(r#"..\assets\terminal_box_icon-dark-48x48.ico"#),
-        None,
-        None,
-    )
-    .unwrap() // unwrap: tested
-}
-
 #[cfg(test)]
 mod test {
-    use crate::{icon_dark, icon_light};
+    use crate::select_icon;
 
     #[test]
     fn check_icons() {
-        icon_light();
-        icon_dark();
+        select_icon(Default::default());
     }
 }
